d72d29718b74cdac6c4f6a3faee4112f1c0ebc20
Adam <Plindzek@users.noreply.github.com>
2020-03-02 13:01:50 +0100

    initial commit

---
+++ src/test/java/io/github/plindzek/FuelCostApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.plindzek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelCostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

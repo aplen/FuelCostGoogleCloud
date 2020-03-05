b219596f01e8d51a4ae830d0a61e2ac2d290416b
Adam <Plindzek@users.noreply.github.com>
2020-03-05 23:06:23 +0100

    implement Spring part1

--- src/main/java/io/github/plindzek/prices/FuelsPriceServlet.java
+++ src/main/java/io/github/plindzek/prices/FuelsPriceServlet.java
@@ -3,39 +3,49 @@ package io.github.plindzek.prices;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-    /**
-     * servlet at the localhost:8080/api/prices
-     * return actual fuel prices
-     * @author Adam
+
+
+/**
+ * servlet at the localhost:8080/api/prices
+ * return actual fuel prices
+ * @author Adam
+ */
+
+@WebServlet(displayName = "Fuels Price Servlet", urlPatterns = {"/api/prices"}, name = "Fuels Price Servlet")
+public class FuelsPriceServlet extends HttpServlet {
+
+    private final Logger logger = LoggerFactory.getLogger(io.github.plindzek.prices.FuelsPriceServlet.class);
+
+    private FuelsPriceService service;
+    private ObjectMapper mapper;
+
+    /*
+     * servlet container needs it
      */
-
-//    @WebServlet(displayName = "Fuels Price Servlet", urlPatterns = {"/api/prices"}, name = "Fuels Price Servlet")
-        @RestController
-    class FuelsPriceServlet {
-
-        private final Logger logger = LoggerFactory.getLogger(io.github.plindzek.prices.FuelsPriceServlet.class);
-
-        private FuelsPriceRepository repository;
-        private ObjectMapper mapper;
-
-             FuelsPriceServlet(FuelsPriceRepository repository, ObjectMapper mapper) {
-            this.repository = repository;
-            this.mapper = mapper;
-        }
-
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-            logger.info("Request got with parameters: " + req.getParameterMap());
-            resp.setContentType("application/json; charset=utf-8");
-
-            mapper.writeValue(resp.getOutputStream(), repository.getPrices());
-
-        }
+    public FuelsPriceServlet() {
+        this(new FuelsPriceService(), new ObjectMapper());
     }
+
+    FuelsPriceServlet(FuelsPriceService service, ObjectMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        logger.info("Request got with parameters: " + req.getParameterMap());
+        resp.setContentType("application/json; charset=utf-8");
+
+        mapper.writeValue(resp.getOutputStream(), service.getPrices());
+
+    }
+}
 

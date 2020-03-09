2aacce192dfadcd420f1c4dae9eae8ffb60790a5
Adam <Plindzek@users.noreply.github.com>
2020-03-09 13:31:20 +0100

    hide unnecessary methods, add comments

--- src/main/java/io/github/plindzek/car/CarRepository.java
+++ src/main/java/io/github/plindzek/car/CarRepository.java
@@ -1,26 +1,19 @@
 package io.github.plindzek.car;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Adam
- * contain methods to execute CRUD operations on car table
- *
+ * Spring Car repository
  */
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
-    List<Car> findAll();
+//  managed by Spring, methods not needed
 
-    Optional<Car> findById(Integer id);
-
-    Car addCar(Car car);
-    boolean deleteCar(Integer id);
-
-//Optional<Car> findByName(String name);
-//Car updateCar(Integer id, String newName);
-
+//  List<Car> findAll();
+//  Optional<Car> findById(Integer id);
+//  boolean deleteCar(Integer id);
+//  Optional<Car> findByName(String name);
+//  Car updateCar(Integer id, String newName);
 }

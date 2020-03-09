fa30a4ace3876e5d773e7ded1e5cb507c97ea67e
Adam <Plindzek@users.noreply.github.com>
2020-03-09 13:22:35 +0100

    add prices loading

--- src/main/java/io/github/plindzek/FuelCostApplication.java
+++ src/main/java/io/github/plindzek/FuelCostApplication.java
@@ -1,5 +1,6 @@
 package io.github.plindzek;
 
+import io.github.plindzek.prices.FuelsPriceScrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FuelCostApplication {
 
 	public static void main(String[] args) {
+		FuelsPriceScrapper.getAutocentrum();
 		SpringApplication.run(FuelCostApplication.class, args);
 	}
 

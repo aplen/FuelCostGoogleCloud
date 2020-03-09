327e51d7c06f18823668fecdfe51c12cd32456da
Adam <Plindzek@users.noreply.github.com>
2020-03-09 14:01:32 +0100

    add coment

--- src/main/java/io/github/plindzek/fuelcost/FuelCost.java
+++ src/main/java/io/github/plindzek/fuelcost/FuelCost.java
@@ -1,6 +1,8 @@
 package io.github.plindzek.fuelcost;
 import io.github.plindzek.car.Car;
-
+/**
+ * @author Adam
+ */
 public interface FuelCost {
 
     double calculateFuelCost(Car car, Trip trip);

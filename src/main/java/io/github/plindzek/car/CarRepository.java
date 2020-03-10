c2d9fc890cd920a4769d67c1b80a5ea228c25ef1
Adam <Plindzek@users.noreply.github.com>
2020-03-10 18:48:00 +0100

    del comments

--- src/main/java/io/github/plindzek/car/CarRepository.java
+++ src/main/java/io/github/plindzek/car/CarRepository.java
@@ -2,18 +2,11 @@ package io.github.plindzek.car;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 /**
  * @author Adam
  * Spring Car repository
  */
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
-//  managed by Spring, methods not needed
-
-//  List<Car> findAll();
-//  Optional<Car> findById(Integer id);
-//  boolean deleteCar(Integer id);
-//  Optional<Car> findByName(String name);
-//  Car updateCar(Integer id, String newName);
+//  managed by Spring, implementation of repository methods not needed
 }

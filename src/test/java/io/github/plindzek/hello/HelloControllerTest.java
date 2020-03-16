1c35269e8c19090cca1f180f14c05b539892c89d
Adam <Plindzek@users.noreply.github.com>
2020-03-16 18:05:21 +0100

    Create HelloControllerTest.java

---
+++ src/test/java/io/github/plindzek/hello/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package io.github.plindzek.hello;
+
+import io.github.plindzek.lang.Lang;
+
+
+public class HelloControllerTest {
+
+    private static final String FALLBACK_NAME = "kitty";
+    private static final Lang FALLBACK_LANG = new Lang(1,"Hello","pl","Name of car: ","LPG l/100km","LPG price: ","km on LPG: ","pb l/100km","PB95 price: ","km on PB95: ","Trip cost: ","calculate","exit","save","use this car");
+
+
+
+
+}

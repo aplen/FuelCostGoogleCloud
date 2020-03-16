51ef7ed8ad071b8f29803e73791043a1e7a85807
Adam <Plindzek@users.noreply.github.com>
2020-03-16 21:53:13 +0100

    add test dependency

--- src/main/java/io/github/plindzek/hello/HelloService.java
+++ src/main/java/io/github/plindzek/hello/HelloService.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 class HelloService {
 
     static final String FALLBACK_NAME = "kitty";
-    static final Lang FALLBACK_LANG = new Lang(1,"Hello","pl","Name of car: ","LPG l/100km","LPG price: ","km on LPG: ","pb l/100km","PB95 price: ","km on PB95: ","Trip cost: ","calculate","exit","save","use this car");
+    static final Lang FALLBACK_LANG = new Lang(1,"Default Welcome","pl","Name of car: ","LPG l/100km","LPG price: ","km on LPG: ","pb l/100km","PB95 price: ","km on PB95: ","Trip cost: ","calculate","exit","save","use this car");
 
     private LangRepository repository;
 
--- src/test/java/io/github/plindzek/hello/HelloControllerTest.java
+++ src/test/java/io/github/plindzek/hello/HelloControllerTest.java
@@ -1,14 +1,65 @@
 package io.github.plindzek.hello;
 
 import io.github.plindzek.lang.Lang;
+import io.github.plindzek.lang.LangRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
 
-    private static final String FALLBACK_NAME = "kitty";
-    private static final Lang FALLBACK_LANG = new Lang(1,"Hello","pl","Name of car: ","LPG l/100km","LPG price: ","km on LPG: ","pb l/100km","PB95 price: ","km on PB95: ","Trip cost: ","calculate","exit","save","use this car");
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HelloService service;
 
 
+    @Test
+    public void shouldReturnExpectedText() throws Exception {
+        when(service.prepareGreeting(null, null)).thenReturn("Siema" + "<br />" + "kitty" + "!");
+
+        mockMvc
+                .perform(get("/api"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Siema" + "<br />" + "kitty" + "!")));
+    }
+
+    @Test
+    public void shouldReturnExpectedTextWithParameters() throws Exception {
+        when(service.prepareGreeting("Adam", 1)).thenReturn("Siema<br />Adam!");
+
+        mockMvc
+                .perform(get("/api")
+                        .param("langId", "1")
+                        .param("name", "Adam"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Siema<br />Adam!")));
+
+//        verify(service, times(1)).prepareGreeting(anyString(), anyInt());
+
+    }
 
 
 }

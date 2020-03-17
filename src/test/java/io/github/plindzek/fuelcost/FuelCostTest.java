e7e39ab444f82b6b07938d3f20b760faae55acc5
Adam <Plindzek@users.noreply.github.com>
2020-03-17 21:10:52 +0100

    Create FuelCostTest.java

---
+++ src/test/java/io/github/plindzek/fuelcost/FuelCostTest.java
@@ -0,0 +1,45 @@
+package io.github.plindzek.fuelcost;
+
+import io.github.plindzek.car.Car;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+public class FuelCostTest {
+    FuelCost fuelCost;
+
+    Car car = new Car();
+    Trip trip = new Trip();
+
+    @Test
+    public void shouldReturnZeroValue(){
+        fuelCost = new NoFuelCost();
+        assertThat(fuelCost.calculateFuelCost(car, trip)).isEqualTo(0.00);
+    }
+
+    @Test
+    public void shouldReturnLpgCostValue(){
+        fuelCost = new LpgCost();
+        car.setLpgOn100Km(10.00);
+        trip.setLpgPrice(2.00);
+        trip.setKmOnLpg(100.00);
+        assertThat(fuelCost.calculateFuelCost(car, trip)).isEqualTo(20.00);
+    }
+    @Test
+    public void shouldReturnOnCostValue(){
+        fuelCost = new OnCost();
+        car.setOnOn100Km(10.00);
+        trip.setOnPrice(2.00);
+        trip.setKmOnOn(100.00);
+        assertThat(fuelCost.calculateFuelCost(car, trip)).isEqualTo(20.00);
+    }
+    @Test
+    public void shouldReturnPbCostValue(){
+        fuelCost = new PbCost();
+        car.setPbOn100Km(10.00);
+        trip.setPbPrice(2.00);
+        trip.setKmOnPb(100.00);
+        assertThat(fuelCost.calculateFuelCost(car, trip)).isEqualTo(20.00);
+    }
+}

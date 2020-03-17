e6bdde85831a8f8377fe172e885109a8f147fe12
Adam <Plindzek@users.noreply.github.com>
2020-03-17 21:11:54 +0100

    split method for easier testing

--- src/main/java/io/github/plindzek/fuelcost/FuelCostService.java
+++ src/main/java/io/github/plindzek/fuelcost/FuelCostService.java
@@ -11,21 +11,26 @@ class FuelCostService {
 
     private FuelCost fuelCost;
 
+  double calcCost(Car car, Trip trip) {
+        this.selectFuel(car);
+        return fuelCost.calculateFuelCost(car, trip);
+    }
+
     /*
-    based on Strategy Pattern
-    proper object is created in dependence of which fuel is chosen
-     */
-    double calcCost(Car car, Trip trip) {
+  based on Strategy Pattern
+  proper object is created in dependence of which fuel is chosen
+   */
+    FuelCost selectFuel(Car car) {
         if (car.isOnPowered()) {
             fuelCost = new OnCost();
+        } else if (car.isLpgPowered()) {
+            fuelCost = new LpgCost();
+        } else if (car.isPbPowered()) {
+            fuelCost = new PbCost();
         } else {
-            if (car.isLpgPowered()) {
-                fuelCost = new LpgCost();
-            } else {
-                fuelCost = new PbCost();
-            }
+            fuelCost = new NoFuelCost();
         }
-        return fuelCost.calculateFuelCost(car, trip);
+        return fuelCost;
     }
 }
 

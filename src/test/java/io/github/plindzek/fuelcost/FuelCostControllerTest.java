9fb4f7a6e5d76636b9f7c4f47ad8d7dbc3e65666
Adam <Plindzek@users.noreply.github.com>
2020-03-18 20:43:38 +0100

    add status ok test

--- src/test/java/io/github/plindzek/fuelcost/FuelCostControllerTest.java
+++ src/test/java/io/github/plindzek/fuelcost/FuelCostControllerTest.java
@@ -1,52 +1,52 @@
 package io.github.plindzek.fuelcost;
 
-import io.github.plindzek.car.Car;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.plindzek.car.CarRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class FuelCostControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
-    //@Autowired to call service methods or @MockBean to use mocked service
     @MockBean
     private FuelCostService fuelCostService;
-
-
+    @MockBean
+    private CarRepository carRepository;
+    private Trip trip;
 
     @Test
     public void shouldReturnExpectedStatus() throws Exception {
-        var car = new Car();
-        var trip = new Trip();
-        //service mocking
-        when(fuelCostService.calcCost(car, trip)).thenReturn(100.00);
+        trip = new Trip();
+        trip.setKmOnLpg(10);
+        trip.setLpgPrice(2);
+        String json = new ObjectMapper().writeValueAsString(trip);
+
+        when(fuelCostService.calcCost(anyInt(), any(Trip.class))).thenReturn(66.25);
 
         mockMvc
-                .perform(get("/api/fuelcost/{id}", anyInt()))
+                .perform(post("/api/fuelcost/{id}", 100)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
                 .andDo(print())
                 .andExpect(status().isOk());
+
+        verify(fuelCostService, times(1)).calcCost(anyInt(), any(Trip.class));
     }
-
-
-
 }
-
-

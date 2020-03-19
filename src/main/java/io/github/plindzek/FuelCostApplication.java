9640c2c64870f0d45841ce7b7c6104858176e118
Adam <Plindzek@users.noreply.github.com>
2020-03-19 23:42:13 +0100

    Update FuelCostApplication.java

--- src/main/java/io/github/plindzek/FuelCostApplication.java
+++ src/main/java/io/github/plindzek/FuelCostApplication.java
@@ -1,19 +1,13 @@
 package io.github.plindzek;
 
-import io.github.plindzek.prices.FuelsPriceScrapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class FuelCostApplication {
 
 	public static void main(String[] args) {
-		FuelsPriceScrapper.getAutocentrum();
+
 		SpringApplication.run(FuelCostApplication.class, args);
 	}
 

2cc987de9ed9ab78a984c346282fe7b3d3771d3d
Adam <Plindzek@users.noreply.github.com>
2020-03-21 19:02:46 +0100

    add date check/add connection or read error handle/refactor

--- src/main/java/io/github/plindzek/prices/FuelsPriceService.java
+++ src/main/java/io/github/plindzek/prices/FuelsPriceService.java
@@ -7,50 +7,77 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 class FuelsPriceService {
-
-    private final Logger logger = LoggerFactory.getLogger(io.github.plindzek.prices.FuelsPriceService.class);
-    private Document doc;
-    private String tempPrice;
+    private final Logger LOGGER = LoggerFactory.getLogger(FuelsPriceService.class);
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    private String dateOfDownload;
+    private boolean freshDataGet = false;
     private List<String> prices = new ArrayList<>();
+    protected static final String AUTOCENTRUM_URL = "http://www.autocentrum.pl/paliwa/ceny-paliw/";
 
     List<String> getPrices() {
+        checkDate(dateOfDownload);
+        if (!freshDataGet) {
+            Document scrappedHtml = scrapAutocentrum();
+            prices.clear();
 
+            prices.add(" Cena LPG: ");
+            prices.add(pickPrice(scrappedHtml.getElementsByAttributeValueContaining("href",
+                    "/paliwa/ceny-paliw/lpg/").select("div").text()));
+
+            prices.add(" Cena PB: ");
+            prices.add(pickPrice(scrappedHtml.getElementsByAttributeValueContaining("href",
+                    "/paliwa/ceny-paliw/pb/").select("div").text()));
+
+            prices.add(" Cena PB Premium: ");
+            prices.add(pickPrice(scrappedHtml.getElementsByAttributeValueContaining("href",
+                    "/paliwa/ceny-paliw/pb-premium").select("div").text()));
+
+            prices.add(" Cena ON: ");
+            prices.add(pickPrice(scrappedHtml.getElementsByAttributeValueContaining("href",
+                    "/paliwa/ceny-paliw/on/").select("div").text()));
+        }
+        return prices;
+    }
+
+    boolean checkDate(String dateOfDownload) {
+
+        String dateNow = dateFormat.format(Calendar.getInstance().getTime());
+        if (dateNow.equals(dateOfDownload)) {
+            freshDataGet = true;
+        } else {
+            freshDataGet = false;
+        }
+        return freshDataGet;
+    }
+
+    Document scrapAutocentrum() {
+        Document scrappedHtml = new Document(AUTOCENTRUM_URL);
         try {
-            doc = Jsoup.connect("https://www.autocentrum.pl/paliwa/ceny-paliw/")
+            scrappedHtml = Jsoup.connect(AUTOCENTRUM_URL)
                     .timeout(5000)
                     .get();
-            logger.info("Prices loaded successfully!");
-        } catch (IOException ole) {
-
-            ole.printStackTrace();
-            logger.error("Prices loading error!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.info("Data download error");
         }
+        LOGGER.info("Data downloaded successfully!");
+        dateOfDownload = dateFormat.format(Calendar.getInstance().getTime());
+        return scrappedHtml;
+    }
 
-        tempPrice = doc.getElementsByAttributeValueContaining("href",
-                "/paliwa/ceny-paliw/lpg/").select("div").text();
-        prices.add(" Cena LPG: ");
-        prices.add(tempPrice.charAt(0) + "." + tempPrice.charAt(2) + tempPrice.charAt(3));
-
-        tempPrice = doc.getElementsByAttributeValueContaining("href",
-                "/paliwa/ceny-paliw/pb/").select("div").text();
-        prices.add(" Cena PB: ");
-        prices.add(tempPrice.charAt(0) + "." + tempPrice.charAt(2) + tempPrice.charAt(3));
-
-        tempPrice = doc.getElementsByAttributeValueContaining("href",
-                "/paliwa/ceny-paliw/pb-premium").select("div").text();
-        prices.add(" Cena PB Premium: ");
-        prices.add(tempPrice.charAt(0) + "." + tempPrice.charAt(2) + tempPrice.charAt(3));
-
-        tempPrice = doc.getElementsByAttributeValueContaining("href",
-                "/paliwa/ceny-paliw/on/").select("div").text();
-        prices.add(" Cena ON: ");
-        prices.add(tempPrice.charAt(0) + "." + tempPrice.charAt(2) + tempPrice.charAt(3));
-
-        return prices;
+    String pickPrice(String tempPrice) {
+        String price = "0.00";
+        try {
+            price = tempPrice.charAt(0) + "." + tempPrice.charAt(2) + tempPrice.charAt(3);
+            freshDataGet = true;
+        } catch (Exception e) {
+            LOGGER.error("Price not found");
+        }
+        return price;
     }
 }

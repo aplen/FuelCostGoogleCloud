cb3fa12e293c1179c13a5fc5661ec62d0def77d8
Adam <Plindzek@users.noreply.github.com>
2020-03-24 00:12:54 +0100

    finish json check in test

--- src/test/java/io/github/plindzek/prices/FuelsPriceControllerTest.java
+++ src/test/java/io/github/plindzek/prices/FuelsPriceControllerTest.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +17,9 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,6 +41,8 @@ public class FuelsPriceControllerTest {
         mockMvc
                 .perform(get("/api/prices"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[\"Test\"]"));
     }
 }

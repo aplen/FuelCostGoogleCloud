fc4a781d613543c0e468579d2297e0376f31f4c1
Adam <Plindzek@users.noreply.github.com>
2020-03-29 20:21:51 +0200

    Create CarControllerTest.java

---
+++ src/test/java/io/github/plindzek/car/CarControllerTest.java
@@ -0,0 +1,50 @@
+package io.github.plindzek.car;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CarControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private CarRepository carRepository;
+
+    @Test
+    public void ShouldReturn() throws Exception {
+Car car = new Car();
+car.setName("Test");
+List <Car> cars = new ArrayList();
+cars.add(car);
+
+        when(carRepository.findAll()).thenReturn(cars);
+
+        mockMvc
+                .perform(get("/api/cars"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("[0].name", containsString("Test")));
+
+    }
+}

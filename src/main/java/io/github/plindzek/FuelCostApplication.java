dfb7f31b5dd40fa41a0cc5b736588e26d09216cf
Adam <Plindzek@users.noreply.github.com>
2020-04-15 18:13:48 +0200

    add H2 console access

--- src/main/java/io/github/plindzek/FuelCostApplication.java
+++ src/main/java/io/github/plindzek/FuelCostApplication.java
@@ -1,7 +1,10 @@
 package io.github.plindzek;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FuelCostApplication {
@@ -9,9 +12,16 @@ public class FuelCostApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(FuelCostApplication.class, args);
+
+	}
+	@Bean
+	ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
 	}
 
-//	@Bean
+	//	@Bean
 //	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 //		return args -> {
 //

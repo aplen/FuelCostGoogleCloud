dac36191fc79577345f0b0fae84851a8f151389e
Adam <Plindzek@users.noreply.github.com>
2020-04-26 13:36:44 +0200

    Update V6__createUsersTable.sql

--- src/main/resources/db/migration/V6__createUsersTable.sql
+++ src/main/resources/db/migration/V6__createUsersTable.sql
@@ -1,5 +1,6 @@
 create table users (
     id int unsigned primary key auto_increment,
+    langid int,
     username varchar(100) not null,
     role varchar(100) not null,
     password varchar(100) not null

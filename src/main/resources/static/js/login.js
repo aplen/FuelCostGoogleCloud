50193cfb4c518f706a9862fb86a4afcfe2078d32
Adam <Plindzek@users.noreply.github.com>
2020-05-21 18:51:22 +0200

    create custom login page

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,73 @@
+    (function main() {
+        //          const API_URL = 'https://wise-mantra-270807.appspot.com/api';
+        const URL = 'http://localhost:8080';
+        const API_URL = 'http://localhost:8080/api';
+        var username;
+
+        showDate();
+        initWelcomeForm();
+
+        function initWelcomeForm() {
+            const CODE_TO_EMOJI = {
+                'pl': '🇵🇱',
+                'en': '🇺🇸',
+                'de': '🇩🇪'
+            };
+            fetch(`${API_URL}/langs`)
+                .then(processOkResponse)
+                .then(langArr => {
+                    document.getElementById('langs').innerHTML = langArr.map(lang => `
+              <label class="pure-radio">
+                <input type="radio" name="lang" value="${lang.langId}">
+                ${CODE_TO_EMOJI[lang.langCode]}
+              </label>
+          `).join('\n');
+                    initWelcomeFormClick();
+                });
+        }
+
+        function initWelcomeFormClick() {
+            const welcomeForm = document.getElementById('welcomeForm');
+
+            document.getElementById('logbtn').addEventListener('click', (event) => {
+
+                fetch(`${URL}/users/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'text/html',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        username: welcomeForm.elements.username.value,
+                        password: welcomeForm.elements.password.value,
+                        langId: welcomeForm.elements.lang.value
+                    })
+                })
+
+                .then(response=>{
+                window.location = `${URL}/index.html`;
+                })
+
+                ;
+
+            });
+        }
+
+        function showDate() {
+            const now = new Date();
+            const currentYear = now.getFullYear().toString();
+            const currentMonth = (now.getMonth() + 1).toString();
+            const currentDay = now.getDate().toString();
+            const formattedDate = `${currentDay.toString().padStart(2, '0')}-${currentMonth.padStart(2, '0')}-${currentYear.padStart(2, '0')}`;
+            document.getElementById('footerDate').innerHTML = "© " + currentYear + " FuelCost, Inc.";
+        }
+
+        function processOkResponse(response = {}) {
+            if (response.ok) {
+                return response.json();
+            }
+            alert(`HTTP status not 200  (${response.status})`);
+            document.forms[0].reset();
+            throw new Error(`Status not 200 (${response.status})`);
+        }
+    })();

ab7d39cab4faf0046845f30f54d3f9d633501607
Adam <Plindzek@users.noreply.github.com>
2020-05-22 21:40:04 +0200

    remove security package

--- src/main/java/io/github/plindzek/SecurityConfig.java
+++ src/main/java/io/github/plindzek/SecurityConfig.java
@@ -1,4 +1,4 @@
-package io.github.plindzek.security;
+package io.github.plindzek;
 
 import io.github.plindzek.appUser.AppUserService;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +29,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/users/login").permitAll()
+                .antMatchers(HttpMethod.POST,"/users/register").permitAll()
                 .antMatchers(HttpMethod.GET,"/users/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/users/loggeduser").hasAnyRole("USER","ADMIN")
 
@@ -38,6 +39,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/cars").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/api/cars/**").hasRole("ADMIN")
 
+                .antMatchers("/register.html").permitAll()
                 .antMatchers("/js/**").permitAll()
                 .antMatchers("/images/**").permitAll()
                 .antMatchers("/css/**").permitAll()

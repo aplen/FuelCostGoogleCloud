c90c183882e8e6cdb7b51b7eddbc54c03b6965b2
Adam <Plindzek@users.noreply.github.com>
2020-05-25 19:40:26 +0200

    add lang endpoint

--- src/main/java/io/github/plindzek/lang/LangController.java
+++ src/main/java/io/github/plindzek/lang/LangController.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,11 @@ class LangController {
     ResponseEntity<List<LangDTO>> findAllLangs() {
         logger.info("Request for all langs got");
         return ResponseEntity.ok(langService.findAll());
+    }
+
+    @GetMapping(value = "/lang")
+    Lang changeLang(@PathVariable Integer langId){
+        return langService.findById(langId);
     }
 
     @GetMapping

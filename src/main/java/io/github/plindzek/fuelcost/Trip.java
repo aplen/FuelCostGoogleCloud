cac73b74bbe8ba0a8e50f81eb6c73dffb475dbe1
Adam Plencler <aplencler@gmail.com>
2021-02-20 10:55:51 +0100

    Update Trip.java

--- src/main/java/io/github/plindzek/fuelcost/Trip.java
+++ src/main/java/io/github/plindzek/fuelcost/Trip.java
@@ -2,6 +2,8 @@ package io.github.plindzek.fuelcost;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 /**
  * @author Adam
  * Temporary Trip object created to collect data of fuel prices and trip length
@@ -10,6 +12,7 @@ import lombok.Data;
 class Trip {
 
     private double kmOnLpg;
+    //@Pattern(regexp = "[0-9]{0,8}\\.{0,1}[0-9]{0,8}$")
     private double kmOnPb;
     private double kmOnOn;
     private double lpgPrice;
